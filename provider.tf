e215590d15137cadced46c28fe7ead70502f7abe
lucabartolotti <lucabartolotti@quantexa.com>
2022-02-15 12:09:45 +0000

    wrong format

--- provider.tf
+++ provider.tf
@@ -1,24 +1,23 @@
 terraform {
 
-  required_version = "~>1.0"
+  required_version =   "~>1.0"
 
-  backend "gcs" {
-    prefix = "compute"
+  backend  "gcs" {
+    prefix  = "compute"
   }
 
   required_providers {
     google = {
       version = "~>4.0"
     }
-    null = {
-      version = "~>3.0"
+      null = {
+    version = "~>3.0"
     }
   }
 
 }
 
 
-
 provider "google" {
-  project = "fake-project"
+  project =  "fake-project"
 }
