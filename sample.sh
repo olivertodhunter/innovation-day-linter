dac971ba3db08a35b17a6dcbcff3f4e6cf5a9b64
ollie <olivertodhunter@quantexa.com>
2022-02-15 14:18:09 +0000

    test files

--- sample.sh
+++ sample.sh
@@ -3,10 +3,10 @@ valid=true
 count=1
 while [ $valid ]
 do
-    echo $count
-    if [ $count -eq 5 ];
-    then
-        break
-    fi
-    ((count++))
+echo $count
+if [ $count -eq 5 ];
+then
+break
+fi
+((count++))
 done
