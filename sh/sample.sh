adec31af619e60cc2aa326d3a18ee487ec80181e
lucabartolotti <lucabartolotti@quantexa.com>
2022-02-15 13:59:32 +0000

    new sample for formatters

---
+++ sh/sample.sh
@@ -0,0 +1,19 @@
+
+__start_kcs() {
+  local cur  prev opts
+   COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}" 
+  prev="${COMP_WORDS[COMP_CWORD - 1]}"
+  # opts="$(kubectl get ns --no-headers=true | cut -d" " -f1| tr "\n" " ")"
+  timeout  5s kubectl get nodes &>/dev/null 
+  timeout=$?
+  if [[ "$timeout" -ne "0" ]]
+   then
+  echo "\n\tconnect to the cluster you stupid\n"
+  return 1
+  else
+    opts="$(kubectl get ns --no-headers=true | cut -d" " -f1)"
+    COMPREPLY=($(compgen -W "${opts}" -- ${cur}))
+     return 0
+  fi
+}
