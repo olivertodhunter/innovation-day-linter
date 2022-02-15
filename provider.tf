a3b30b8fd742049464156852e67980430fcab1de
lucabartolotti <lucabartolotti@quantexa.com>
2022-02-15 10:43:36 +0000

    fake terraform file with errors

---
+++ provider.tf
@@ -0,0 +1,23 @@
+terraform {
+
+  required_version =   "~>1.0"
+
+  backend  "gcs" {
+    prefix  = "compute"
+  }
+
+  required_providers {
+    google = {
+      version = "~>4.0"
+    }
+      null = {
+    version = "~>3.0"
+    }
+  }
+
+}
+
+
+provider "google" {
+  project =  "fake-project"
+}
