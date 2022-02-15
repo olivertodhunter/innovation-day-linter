ff83dacbde9bcf1f07bbe939177ea2f48bb30e21
Terraform format <oliver-todhunter@users.noreply.github.com>
2022-02-15 15:28:30 +0000

    Formatting

--- Jenkinsfile
+++ Jenkinsfile
@@ -5,8 +5,8 @@ pipeline {
    stages {
      stage('build') {
            steps {              sh 'node --version'
-            }
-        }
-    }
+           }
+     }
+   }
 }
 
--- sample.tf
+++ sample.tf
@@ -1,5 +1,5 @@
 provider "google" {
-project =  "{{YOUR GCP PROJECT}}"
- region      = "us-central1"
- zone    = "us-central1-c"
+  project = "{{YOUR GCP PROJECT}}"
+  region  = "us-central1"
+  zone    = "us-central1-c"
 }
